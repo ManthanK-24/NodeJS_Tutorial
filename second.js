abe049110dafe654d485898910d0715c40596fb3
ManthanK-24 <manthandec@gmail.com>
2023-09-28 17:00:01 +0530

    Some Fav Players Added

--- second.js
+++ second.js
@@ -1,12 +1,20 @@
+// (function(exports,require,__filename,__dirname){
+
 mantobj = {
     name: "Manthan",
     favNum : 1,
-    developer: true
+    developer: true,
+    favPlayer : { 
+                p1:"Vijay Shekhar Sharma", 
+                p2:"Atlassian CEO"
+               },
 }
 
-console.log('x',exports)
-console.log('y',require)
-console.log('z',__filename)
-console.log('a',__dirname)
+// console.log('x',exports)
+// console.log('y',require)
+// console.log('z',__filename)
+// console.log('a',__dirname)
 
 module.exports = mantobj;
+
+// })
